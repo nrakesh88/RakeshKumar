0dea0e544fcee64e856b5833c67fe9541094506f
Gopinath Jayakumar <gopinath@testleaf.com>
2017-05-27 20:27:03 +0530

    Week1Day1 Codes

---
+++ SelMay2017/src/main/java/week1Day1/HelloWorld.java
@@ -0,0 +1,42 @@
+package week1Day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld {
+	
+	HelloWorld(){
+		
+	}
+
+	public static void main(String[] args) {
+
+		HelloWorld txt = new HelloWorld();
+		txt.b();
+		ChromeDriver driver = new ChromeDriver();
+		System.out.println("Karim is owner of TestLeaf");
+//		b();
+	}
+
+	public void a(){
+		b();
+		LearnIns x = new LearnIns();
+		x.getName();
+		main(null);
+	}
+
+	public void b(){
+
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
