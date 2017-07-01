5a970753321444a93d5e8368131485079341dfb7
Gopinath Jayakumar <gopinath@testleaf.com>
2017-07-01 19:29:37 +0530

    Week5Day1

--- SelMay2017/src/main/java/week2Day1/LearnAlert.java
+++ SelMay2017/src/main/java/week2Day1/LearnAlert.java
@@ -1,19 +1,29 @@
 package week2Day1;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 public class LearnAlert {
 	
-	public static void main(String[] args) throws InterruptedException {
+	@Test
+	public void alert() throws InterruptedException, MalformedURLException {
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("firefox");
+		dc.setPlatform(Platform.WINDOWS);
 		
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		
-		ChromeDriver driver = new ChromeDriver();
-	//	FirefoxDriver driver = new FirefoxDriver();
-		
+		RemoteWebDriver driver = new
+				RemoteWebDriver(new URL(
+						"http://192.168.1.73:4444/wd/hub"), dc);
+	
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.irctc.co.in/eticketing/loginHome.jsf");
--- SelMay2017/src/main/java/week2Day1/LearnWindows.java
+++ SelMay2017/src/main/java/week2Day1/LearnWindows.java
@@ -1,15 +1,30 @@
 package week2Day1;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Set;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 public class LearnWindows {
 
-	public static void main(String[] args) {
+	@Test
+	public void windows() throws MalformedURLException {
+		
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		dc.setPlatform(Platform.WINDOWS);
+		
+		RemoteWebDriver driver = new
+				RemoteWebDriver(new URL(
+						"http://192.168.1.73:4444/wd/hub"), dc);
 
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");		
-		ChromeDriver driver = new ChromeDriver();		
+		/*System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");		
+		ChromeDriver driver = new ChromeDriver();	*/	
 		driver.manage().window().maximize();		
 		driver.navigate().to("http://legacy.crystalcruises.com/");
 	//	String parentWindow = driver.getWindowHandle();
